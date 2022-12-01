d0a1b23a4a447fac9bd2934640b69353711a8ad1
kuba <szachmistrz2@gmail.com>
2022-12-01 22:52:41 +0000

    JoinScreen removed

--- src/screens/homeScreen.js
+++ src/screens/homeScreen.js
@@ -20,65 +20,6 @@ import {
   useParticipant,
 } from '@videosdk.live/react-native-sdk';
 
-function JoinScreen(props) {
-  const [meetingVal, setMeetingVal] = useState('');
-  return (
-    <SafeAreaView
-      style={{
-        flex: 1,
-        backgroundColor: '#F6F6FF',
-        justifyContent: 'center',
-        paddingHorizontal: 6 * 10,
-      }}>
-      <TouchableOpacity
-        onPress={() => {
-          props.getMeetingId();
-        }}
-        style={{backgroundColor: '#1178F8', padding: 12, borderRadius: 6}}>
-        <Text style={{color: 'white', alignSelf: 'center', fontSize: 18}}>
-          Create Meeting
-        </Text>
-      </TouchableOpacity>
-
-      <Text
-        style={{
-          alignSelf: 'center',
-          fontSize: 22,
-          marginVertical: 16,
-          fontStyle: 'italic',
-          color: 'grey',
-        }}>
-        ---------- OR ----------
-      </Text>
-      <TextInput
-        value={meetingVal}
-        onChangeText={setMeetingVal}
-        placeholder={'XXXX-XXXX-XXXX'}
-        style={{
-          padding: 12,
-          borderWidth: 1,
-          borderRadius: 6,
-          fontStyle: 'italic',
-        }}
-      />
-      <TouchableOpacity
-        style={{
-          backgroundColor: '#1178F8',
-          padding: 12,
-          marginTop: 14,
-          borderRadius: 6,
-        }}
-        onPress={() => {
-          props.getMeetingId(meetingVal);
-        }}>
-        <Text style={{color: 'white', alignSelf: 'center', fontSize: 18}}>
-          Join Meeting
-        </Text>
-      </TouchableOpacity>
-    </SafeAreaView>
-  );
-}
-
 const Button = ({onPress, buttonText, backgroundColor}) => {
   return (
     <TouchableOpacity
@@ -184,7 +125,7 @@ function ParticipantList({participants}) {
 function MeetingView() {
   const {join, leave, toggleWebcam, toggleMic, meetingId, participants} =
     useMeeting({});
-  const participantsArrId = [...participants.keys()]; // Add this line
+  const participantsArrId = [...participants.keys()];
 
   return (
     <View style={{flex: 1}}>
@@ -210,22 +151,24 @@ const CallRoute = () => {
     setMeetingId(meetingId);
   };
 
-  return meetingId ? (
-    <SafeAreaView style={{flex: 1, backgroundColor: '#F6F6FF'}}>
-      <MeetingProvider
-        config={{
-          meetingId,
-          micEnabled: false,
-          webcamEnabled: true,
-          name: 'Test User',
-        }}
-        token={token}>
-        <MeetingView />
-      </MeetingProvider>
-    </SafeAreaView>
-  ) : (
-    <JoinScreen getMeetingId={getMeetingId} />
-  );
+  if (meetingId) {
+    return (
+      <SafeAreaView style={{flex: 1, backgroundColor: '#F6F6FF'}}>
+        <MeetingProvider
+          config={{
+            meetingId,
+            micEnabled: false,
+            webcamEnabled: true,
+            name: 'Test User',
+          }}
+          token={token}>
+          <MeetingView />
+        </MeetingProvider>
+      </SafeAreaView>
+    );
+  } else {
+    getMeetingId();
+  }
 };
 
 const MatchesRoute = () => <Text>Matches</Text>;
